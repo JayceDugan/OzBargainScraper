22eca593b7ff1b402bd0933433945a8189a7b37d
Jayce Dugan <jayced@aspedia.net>
2021-01-24 17:27:53 +0000

    refactor: Listings now apply url shortener and consider formatting.
    - Character limitations also applied as Twilio restricts text messages
    to 1600 characters. (This is handled by the recursive check found in list.)

--- src/classes/TeaserListings.py
+++ src/classes/TeaserListings.py
@@ -1,10 +1,14 @@
+import pyshorteners
 from .TeaserInstance import TeaserInstance
+from ..lib.config import characterLimit
 
 class TeaserListings:
   def __init__(self, teasers):
     self.teasers = teasers
     self.teaserInstances = []
     self.list = self.list
+    self.print = self.print
+    self.urlShortener = pyshorteners.Shortener()
 
     self.build_teaser_instances()
 
@@ -12,13 +16,29 @@ class TeaserListings:
     for teaser in self.teasers:
       self.teaserInstances.append( TeaserInstance(teaser) )
 
-  def list(self):
+  def list(self, limit = 15):
+    result = ''
     total = len(self.teaserInstances)
-    print(str(total) + ' OzBargain deals found.')
-    print("\n")
+
+    result += str(total) + ' OzBargain deals today: '
+    result += '\n'
+    result += '\n'
 
     for teaser in self.teaserInstances:
       if teaser.title and teaser.link:
-        print(teaser.title)
-        print(teaser.link)
-        print('\n')
+        result += " ".join(teaser.title.split()[:limit])
+        result += '\n'
+        result += self.urlShortener.tinyurl.short(teaser.link)
+        result += '\n'
+        result += '\n'
+
+    if (self.character_limit_exceeded(result)):
+      return self.list(limit - 1)
+    else:
+      return result
+
+  def character_limit_exceeded(self, payload):
+    return len(payload) > characterLimit
+
+  def print(self):
+    print(self.list())
