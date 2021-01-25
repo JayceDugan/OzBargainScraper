60361fc1d9ac227b533c8e54f36ec1a8a7111774
Jayce Dugan <jayced@aspedia.net>
2021-01-25 22:47:15 +0000

    refactor: Method names updated for internal notifications.py methods.

--- notifications.py
+++ notifications.py
@@ -3,13 +3,12 @@ import twilio_client
 
 class Notifications:
   def __init__(self):
-    if config.twilio_active:
-      self.twilio_client = twilio_client.TwilioClient()
+    self.twilio_client = twilio_client.TwilioClient()
 
   def check(self, payload):
     if payload == 'twilio':
-      self.check_twilio()
+      self.notify_twilio()
 
-  def check_twilio(self):
+  def notify_twilio(self):
     if config.twilio_active:
       self.twilio_client.notify(config.twilio_notification_message)
